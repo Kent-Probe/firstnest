b48663390c6fa849fa42a21ca65710280b91ad92
Kent-Probe <kevinshe01@gmail.com>
2024-03-22 17:13:22 -0500

    nest with mongoose

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,21 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { HousesController } from './houses/houses.controller';
+import { HousesService } from './houses/houses.service';
+import { UsersModule } from './users/users.module';
+import { HousesModule } from './houses/houses.module';
+import { MongooseModule } from '@nestjs/mongoose';
+import * as dotenv from 'dotenv';
+dotenv.config();
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+   imports: [
+      UsersModule,
+      HousesModule,
+      MongooseModule.forRoot(process.env.DB_URL),
+   ],
+   controllers: [AppController, HousesController],
+   providers: [AppService, HousesService],
 })
 export class AppModule {}
--- src/app.service.ts
+++ src/app.service.ts
@@ -2,7 +2,12 @@ import { Injectable } from '@nestjs/common';
 
 @Injectable()
 export class AppService {
-  getHello(): string {
-    return 'Hello World!';
-  }
+   getHello(name: string): string {
+      if (name == null) return `Hello World!`;
+      return `Hello ${name}`;
+   }
+
+   sayGoodbay(): string {
+      return 'Bye! bye!';
+   }
 }
