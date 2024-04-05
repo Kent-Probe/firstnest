a2f0daeec515267ee4d9676c7757540350d2308d
Kent-Probe <kevinshe01@gmail.com>
2024-04-05 17:54:24 -0500

    token of jsonwebtoken

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,6 +1,7 @@
 import { HttpException, HttpStatus, Injectable } from '@nestjs/common';
 import { UsersService } from 'src/users/users.service';
 import * as bcrypt from 'bcrypt';
+import * as jwt from 'jsonwebtoken';
 
 @Injectable()
 export class AuthService {
@@ -9,10 +10,25 @@ export class AuthService {
    async validateUser(email: string, password: string): Promise<any> {
       const user = await this.userService.findByEmail(email);
       if (user && bcrypt.compareSync(password, user.password)) {
+         const token = jwt.sign(
+            {
+               id: user.id,
+               email: user.email,
+               rol: user.rol,
+               avatar: user.avatar,
+               name: user.name,
+               lastname: user.lastname
+            },
+            process.env.JWT_SECRET,
+            {
+               expiresIn: 3600
+            }
+         )
          return {
             status: 'success',
             code: 200,
             message: 'login successful',
+            token: token
          };
       }
       throw new HttpException(
